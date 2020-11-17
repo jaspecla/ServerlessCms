d403109fffd0a0793a505e5946b91f906aef0a07
Jason Specland <jaspecla@microsoft.com>
2020-11-16 19:48:59 -0500

    created new/edit article page

---
+++ ServerlessCms.EditorApp/Pages/NewEditArticle.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using ServerlessCms.DTO;
+using ServerlessCms.EditorApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerlessCms.EditorApp.Pages
+{
+  public partial class NewEditArticle 
+  {
+    [Inject]
+    protected ArticleService ArticleService { get; set; }
+
+    public Article Article { get; set; } = new Article();
+
+    [Parameter]
+    public string Id { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+      return base.OnInitializedAsync();
+    }
+
+  }
+}
